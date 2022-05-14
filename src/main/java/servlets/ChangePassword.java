55c8bc770869ddfc08f457ee90dd9c12c9901325
Abhinov <abhinov.in@gmail.com>
2022-05-14 12:15:12 +0530

    migrated from java 11 to 8; support javax tomcat downgraded to 9

--- src/main/java/servlets/ChangePassword.java
+++ src/main/java/servlets/ChangePassword.java
@@ -1,9 +1,9 @@
 package servlets;
 
 
-import jakarta.servlet.http.HttpServlet;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import java.io.*;
 
--- src/main/java/servlets/Login.java
+++ src/main/java/servlets/Login.java
@@ -1,8 +1,8 @@
 package servlets;
 
-import jakarta.servlet.http.HttpServlet;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
 import java.io.PrintWriter;
